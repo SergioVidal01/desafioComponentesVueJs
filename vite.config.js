36f442bce75c6a0c8a8fcdbce5cffdd5c9c8e8c9
Sergio Vidal <s.vidal.bravo.92@gmail.com>
2024-11-28 00:30:17 -0300

    git

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: '/desafioComponentesVueJs/' // reemplaza 'nombre-de-tu-repositorio' por el nombre de tu repo
+});
